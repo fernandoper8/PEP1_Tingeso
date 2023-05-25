bdce0025e5647d5f7dfa2a40179d851eb28b0cff
Fernando <fernando.perez.c@usach.cl>
2023-05-24 21:48:57 -0400

    Cambios import.sql para testear

--- milkStgo/src/main/resources/import.sql
+++ milkStgo/src/main/resources/import.sql
@@ -1,5 +1,11 @@
-INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('1003', 'A', 'Armin van Buuren', 'Si');
-INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('2001', 'B', 'Oliver Heldens', 'No');
+INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('1003', 'A', 'Armin van Buuren', 'No');
+INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('2001', 'B', 'Oliver Heldens', 'Si');
+INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('3001', 'A', 'Colun', 'No');
+INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('4001', 'B', 'Dom Dolla', 'Si');
+INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('5001', 'A', 'Mark Kinchen', 'No');
+INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('6001', 'C', 'Jamie Jones', 'Si');
+INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('7001', 'B', 'John Summit', 'Si');
+INSERT INTO proveedor(codigo, categoria, nombre, retencion) VALUES ('8001', 'A', 'Joris Voorn', 'No');
 
 INSERT INTO acopio(id_acopio, fecha, id_proveedor, kls_leche, turno) VALUES ('1', '17-03-2023', '1003', '1700', 'M');
 INSERT INTO acopio(id_acopio, fecha, id_proveedor, kls_leche, turno) VALUES ('2', '17-03-2023', '1003', '120', 'M');
